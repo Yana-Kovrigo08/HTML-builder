039f599970e2768c602c97b02c96f971a5989536
Yana-Kovrigo08 <yana_kovrigo@mail.ru>
2022-11-07 17:59:24 +0000

    feat: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+//Импорт всех требуемых модулей
+const path = require("path");
+const fs = require("fs/promises");
+const dir = path.join(__dirname, "files");
+const dircopy = path.join(__dirname, "files-copy");
 
+
+fs.rm(dircopy, {
+  recursive: true,
+  force: true,
+}).finally(function () {
+  fs.mkdir(dircopy, {
+    recursive: true,
+  });
+  fs.readdir(dir, {
+    withFileTypes: true,
+  }).then(function (data) {
+    data.forEach(function (item) {
+      if (item.isFile()) {
+        const Item = path.join(dir, item.name);
+        const ItemDes = path.join(dircopy, item.name);
+        fs.copyFile(Item, ItemDes);
+      }
+    });
+  });
+});
